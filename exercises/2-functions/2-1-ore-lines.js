ad292b80656594cd85a9ea5e3cff50a41a38e4c9
Achille Ernould <achille.ernould@student.ehb.be>
2022-10-12 11:10:18 +0200

    exercise 2-1 done

---
+++ exercises/2-functions/2-1-ore-lines.js
@@ -0,0 +1,47 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+let width = context.canvas.width;
+
+let height = context.canvas.height;
+
+let margin = 50;
+
+let spacing = 100;
+
+
+
+drawLine();
+
+
+function drawLine() {
+
+context.beginPath();
+context.lineWidth = 8;
+context.moveTo(margin, spacing);
+context.lineTo(width - margin, spacing);
+context.lineTo(margin, spacing * 2);
+context.moveTo(margin, spacing * 2);
+context.lineTo(width - margin, spacing * 2);
+context.lineTo(margin, spacing * 3);
+context.moveTo(margin, spacing * 3);
+context.lineTo(width - margin, spacing * 3);
+context.lineTo(margin, spacing * 4);
+context.moveTo(margin, spacing * 4);
+context.lineTo(width - margin, spacing * 4);
+context.lineTo(margin, spacing * 5);
+context.moveTo(margin, spacing * 5);
+context.lineTo(width - margin, spacing * 5);
+context.lineTo(margin, spacing * 6);
+context.moveTo(margin, spacing * 6);
+context.lineTo(width - margin, spacing * 6);
+context.stroke();
+
+
+
+
+}
